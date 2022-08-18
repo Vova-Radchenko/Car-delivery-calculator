0a4112dbb3e3ac69bbd6afc1026318f76c4d89ec
Vova-Radchenko <vettel070897@gmail.com>
2022-08-18 22:16:55 +0300

    start create data access

---
+++ CarDeliveryCalculator/CarDeliveryCalculator.DataAccess/Repositories/Implementation/CarRepository.cs
@@ -0,0 +1,38 @@
+﻿using CarDeliveryCalculator.DataAccess.Entities;
+using CarDeliveryCalculator.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDeliveryCalculator.DataAccess.Repositories.Implementation
+{
+    public class CarRepository : ICarRepository
+    {
+        public Task AddAsync(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Car>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Car> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CarDeliveryCalculator/CarDeliveryCalculator.DataAccess/Repositories/Interfaces/ICarRepository.cs
@@ -0,0 +1,16 @@
+﻿using CarDeliveryCalculator.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CarDeliveryCalculator.DataAccess.Repositories.Interfaces
+{
+    public interface ICarRepository
+    {
+        Task AddAsync(Car car);
+        Task<ICollection<Car>> GetAllAsync();
+        Task<Car> GetByIdAsync(int id);
+        Task UpdateAsync(Car car);
+        Task DeleteAsync(Car car);
+    }
+}
